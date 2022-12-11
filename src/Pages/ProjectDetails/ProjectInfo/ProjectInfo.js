ad7d276d3f64a61d8042f32ff0287a4fe4b92f34
euhansarkar <euhansarkar@gmail.com>
2022-12-11 23:47:46 +0600

    website almost ready for deploy

--- src/Pages/ProjectDetails/ProjectInfo/ProjectInfo.js
+++ src/Pages/ProjectDetails/ProjectInfo/ProjectInfo.js
@@ -1,8 +1,9 @@
 import { data } from "autoprefixer";
+import { stringify } from "postcss";
 import React from "react";
 
 const ProjectInfo = ({ data }) => {
-  const { features, startDate, endDate, name, description, links } = data;
+  const { features, technologies, startDate, endDate, name, description, links } = data;
   console.log(data);
   return (
     <div className="hero">
@@ -12,7 +13,12 @@ const ProjectInfo = ({ data }) => {
           <h2 className="py-3 font-bold">Project Started: {startDate}</h2>
           
           <h2 className="py-3 font-bold">Project Ended: {endDate}</h2>
-          <p>{description}</p>
+            <h2 className="text-3xl mb-2 font-bold ">technologies that i used: </h2>
+          <div className="grid grid-cols-2 gap-4">
+            {
+              technologies.map(techno => <strong><p>{techno}</p></strong>)
+            }
+          </div>
         </div>
         <div className="w-full md:w-8/12">
           <h1 className="text-5xl font-bold">{name}</h1>
